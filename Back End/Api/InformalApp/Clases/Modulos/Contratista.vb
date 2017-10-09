822e4a6af272786bf2aba8c0a0a36ce2d6119ba8
G3RMAN <32443291+G3RMAN@users.noreply.github.com>
2017-10-09 09:56:57 -0500

    Módulo para el crud de las clases.

---
+++ Back End/Api/InformalApp/Clases/Modulos/Contratista.vb
@@ -0,0 +1,31 @@
+﻿Public Class DbContratista
+    'Obtiene todos los contratistas de db'
+    Function Obtener() As Integer
+
+        Return 0
+    End Function
+
+    'Obtiene un contratista por id desde db'
+    Function ObtenerId() As Integer
+
+        Return 0
+    End Function
+
+    'inserta uno o varios contratistas a db'
+    Function Insertar() As Integer
+
+        Return 0
+    End Function
+
+    'elimina uno o varios contratistas de db'
+    Function Eliminar() As Integer
+
+        Return 0
+    End Function
+
+    'actualiza uno o varios contratistas a db'
+    Function Actualizar() As Integer
+
+        Return 0
+    End Function
+End Class
