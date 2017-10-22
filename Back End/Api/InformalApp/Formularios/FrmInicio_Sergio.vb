cd6e5f8c4e4b28c699f568504936d4545ed87cd2
SerggioAn27 <32622286+SerggioAn27@users.noreply.github.com>
2017-10-22 00:27:10 -0500

    Inicio Form

---
+++ Back End/Api/InformalApp/Formularios/FrmInicio_Sergio.vb
@@ -0,0 +1,9 @@
+﻿Public Class FrmInicio_Sergio
+    Private Sub Button_FrmLabor_Click(sender As Object, e As EventArgs) Handles Button_FrmLabor.Click
+
+    End Sub
+
+    Private Sub Button_FrmContratista_Click(sender As Object, e As EventArgs) Handles Button_FrmContratista.Click
+
+    End Sub
+End Class
